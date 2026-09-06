4f56cdc844ee4f0c92448b919b473123f3da8686
Harris <hdentry56@gmail.com>
2026-09-06 11:27:16 +1000

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d "%~dp0"
+echo Syncing dependencies from Pipfile.lock...
+call pipenv sync
+if errorlevel 1 exit /b 1
+
 echo Starting Status Updater (pipenv run python main.py)...
 echo.
 call pipenv run python main.py
